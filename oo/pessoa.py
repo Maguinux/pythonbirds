c5cdb5e477df39c65153fc831249ab793dcf2eae
Maguinux <1.no.name.mrx@gmail.com>
2020-04-07 20:23:17 -0300

    criados atributos de instãncia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,20 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=35 ):
+        self.idade = idade
+        self.nome = nome
+
+    def comprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('dean')
+    print(Pessoa.comprimentar(p))
+    print(id(p))
+    print(p.comprimentar())
+    print(p.nome)
+    p.nome = 'Jackson'
+    print(p.nome)
+    print(p.idade)
+
 
 
